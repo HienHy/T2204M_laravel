ea09c57b6811001bab21362fc0792fed5503023e
hienhy <101379678+HienHy@users.noreply.github.com>
2023-02-11 18:29:24 +0700

    commit 1

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,18 @@
+@extends("admin.layout")
+<!-- tham so 1 lua chon, tham so 2 thay doi -->
+@section("title","Ve chung toi")
+
+@section("content-header")
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1 class="m-0 text-dark">About-Us</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+@endsection
+
---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield("title","Trang quan tri he thong")</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @yield("before_css")
+
+@include('admin.html.css')
+    @yield("after_css")
+
+
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+    @include('admin.html.nav')
+
+
+
+    @include('admin.html.aside')
+
+    <!-- Main Sidebar Container -->
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                @yield("content-header")
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+              @yield('main-content')
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+
+    <!-- /.control-sidebar -->
+    @include('admin.html.footer')
+
+</div>
+<!-- ./wrapper -->
+@yield("before_js")
+@include('admin.html.js')
+@yield("after_js")
+
+<!-- jQuery -->
+</body>
+</html>
